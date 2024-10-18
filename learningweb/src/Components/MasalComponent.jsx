0b139f0786ddc2f4dec66e907fb38ab30a7bebce
Shiva Saini <factwiths@gmail.com>
2024-10-18 20:03:13 +0530

    final

--- learningweb/src/Components/MasalComponent.jsx
+++ learningweb/src/Components/MasalComponent.jsx
@@ -14,7 +14,7 @@ const AnimatedMasalComponent = () => {
     }, [isInView, controls]);
 
     return (
-        <div ref={ref} className="bg-[#571937] min-h-[550px] flex flex-col text-white text-center relative overflow-hidden px-4 py-8 md:px-8 md:py-12">
+        <div ref={ref} className="bg-[#571937] min-h-[500px] flex flex-col text-white text-center relative overflow-hidden px-4 py-8 md:px-8 md:py-12">
             <div className="flex flex-col justify-start items-center mb-8 md:mb-12">
                 <p className="text-sm md:text-base lg:text-lg mb-2">
                     Explore our app to keep the
@@ -26,30 +26,20 @@ const AnimatedMasalComponent = () => {
                     as you discover other features that enhance your learning journey and ignite your passion for growth!
                 </p>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center relative">
                 <motion.div
                     initial="hidden"
                     animate={controls}
                     variants={{
                         visible: { y: 0, opacity: 1, transition: { duration: 2 } },
-                        hidden: { y: "100%", opacity: 0 },
+                        hidden: { y: "100%", opacity: 0 }  // Start from the bottom
                     }}
-                    className="relative flex flex-col justify-center items-center mb-8 md:mb-12"
+                    className="absolute bottom-10 flex flex-col justify-center items-center mb-8 md:mb-12"
                 >
-                    {/* <motion.img
-                        variants={{
-                            visible: { opacity: 1, scale: 1, transition: { delay: 1, duration: 1.5 } },
-                            hidden: { opacity: 0, scale: 0.5 },
-                        }}
-                        src="animation.gif"
-                        alt="Flame"
-                        className="absolute w-[300px] h-[70px] md:w-[600px] md:h-[250px] lg:w-[800px] lg:h-[450px] bottom-full mb-[-100px] md:mb-[-200px] lg:mb-[-300px] transform -translate-x-1/2"
-                    /> */}
-
                     <img
-                        src="images/masal.png"
+                        src="images/flamebg.png"
                         alt="Flame body"
-                        className="w-32 md:w-28 lg:w-34 mb-[-100px] md:mb-[-200px] lg:mb-[-300px] -z-10"
+                        className="-mb-[170px]"
                     />
                 </motion.div>
 
